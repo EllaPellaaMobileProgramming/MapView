f19e7969105a70b3d8eae4500a27a1a99b9232a0
EllaPellaa <112462853+EllaPellaa@users.noreply.github.com>
2025-02-11 20:38:34 +0200

    Map works, one marker can be added

--- App.js
+++ App.js
@@ -2,13 +2,63 @@ import { StatusBar } from 'expo-status-bar';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 import Map from './screens/Map';
+import { useState } from 'react';
+import * as Location from 'expo-location'
+import { PaperProvider } from 'react-native-paper';
+import MainAppBar from './components/MainAppBar';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const settings = {
+  backgroundColor: '#00a484'
+}
+
+const icons = {
+  location_not_known: 'crosshairs',
+  location_searching: 'crosshairs-question',
+  location_found: 'crosshairs-gps'
+}
 
 export default function App() {
+  //Variable for icon
+  const [icon, setIcon] = useState(icons.location_not_known)
+  //Variable for location
+  const [location, setLocation] = useState({
+    latitude: 65.0800,
+    longitude: 25.4800,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421
+  })
+
+  //Ask for permission for location
+  const getUserPosition = async () => {
+    setIcon(icons.location_searching)
+    let {status} = await Location.requestForegroundPermissionsAsync()
+
+    try {
+      if (status !== 'granted') {
+        console.log('Geolocation failed')
+        return
+      }
+      const position = await Location.getCurrentPositionAsync({accuracy: Location.Accuracy.High})
+      setLocation({...location, "latitude": position.coords.latitude, "longitude": position.coords.longitude})
+      setIcon(icons.location_found)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Map />
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <MainAppBar 
+        title="Map"
+        backgroundColor={settings.backgroundColor}
+        icon={icon}
+        getUserPosition={getUserPosition}
+      />
+      <SafeAreaView style={styles.container}>
+        <Map location={location} />
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
--- screens/Map.js
+++ screens/Map.js
@@ -1,14 +1,44 @@
+import { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from 'expo-location'
 
-export default function Map() {
+export default function Map(props) {
+
+  const [marker, setMarker] = useState(null)
+
+  //UseEffect
+  /*useEffect(() => {
+    (async() => {
+      getUserPosition()
+    })()
+  }, [])*/
+
+  //Make a marker
+  const showMarker = (e) => {
+    const coords = e.nativeEvent.coordinate
+    setMarker(coords)
+  }
+
+  //Return
   return (
     <MapView 
       style={styles.map}
-    />
+      region={props.location}
+      mapType="satellite"
+      onLongPress={showMarker}
+    >
+      {marker &&
+        <Marker 
+          title="My marker" 
+          coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
+        />
+      }
+    </MapView>
   )
 }
 
+//Stylesheet
 const styles = StyleSheet.create({
   map:{
     height: '100%',
